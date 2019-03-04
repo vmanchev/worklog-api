35dc1078ba7c5ce1815d47eb50a560bf9339d6e0
Venelin Manchev <venelinphp@gmail.com>
2019-03-04 18:47:48 +0200

    feat: secured log with roles

--- app/controllers/LogController.php
+++ app/controllers/LogController.php
@@ -4,7 +4,7 @@ namespace Worklog\Controllers;
 
 use Worklog\Controllers\BaseController;
 use Worklog\Models\Log as LogModel;
-use Worklog\Models\Project as ProjectModel;
+use Worklog\Models\ProjectTeam as TeamModel;
 
 class LogController extends BaseController
 {
@@ -12,6 +12,10 @@ class LogController extends BaseController
     public function create()
     {
         $logData = (array) $this->request->getJsonRawBody();
+
+        if (!TeamModel::isTeamMember($logData['project_id'], $this->auth->data('user')->id)) {
+            return $this->errorResponse(null, 403);
+        }
 
         // make sure time is logged for the current user
         $logData['user_id'] = $this->auth->data('user')->id;
@@ -27,91 +31,86 @@ class LogController extends BaseController
 
     /**
      * Search logs by project and/or user
-     * 
+     *
      * When no user_id is provided, logs for all users will be returned.
      * When user_id is provided, only the related logs will be returned.
-     * 
+     *
      * When project_id is provided, only logs for the related project will be returned.
      * When project_id is not provided, only logs for projects where current user
      * paraticipates (either as user or admin), will be returned.
      */
-    public function search()
+    public function search(int $project_id)
     {
         $queryParams = $this->request->getQuery();
 
         // remove _url which is always the first element
         array_shift($queryParams);
-        
-        if(!empty($queryParams)) {
-          return $this->successResponse(LogModel::searchWithParams($queryParams));
+
+        if (!empty($queryParams)) {
+            return $this->successResponse(LogModel::searchWithParams($queryParams));
         }
 
         return $this->successResponse(
-          (new LogModel())->searchWithinParticipatingProjects($this->auth->data('user')->id)
+            (new LogModel())->searchWithinParticipatingProjects($this->auth->data('user')->id)
         );
     }
 
     /**
      * Deletes a log entry
-     * 
+     *
      * It should either the log entry author or project admin
-     * 
+     *
      * @params int $id
      */
-    public function delete(int $id) {
+    public function delete(int $id)
+    {
 
-      $log = LogModel::findFirst($id);
+        $log = LogModel::findFirst($id);
 
-      // invalid id
-      if (!$log) {
-        return $this->errorResponse(new LogModel(), 404);
-      }
-      
-      // current user is worklog author
-      if ($log->user_id === $this->auth->data('user')->id) {
-        return $this->deleteLog($log);
-      }
+        // invalid id
+        if (!$log) {
+            return $this->errorResponse(new LogModel(), 404);
+        }
 
-      // find the project owner
-      $project = ProjectModel::findFirst($log->project_id);
+        // current user is worklog author or current user is team admin
+        if ($log->user_id === $this->auth->data('user')->id || TeamModel::isTeamAdmin($log->project_id, $this->auth->data('user')->id)) {
+            return $this->deleteLog($log);
+        }
 
-      // delete only if current user is project owner
-      if ($project->user_id === $this->auth->data('user')->id) {
-        return $this->deleteLog($log);
-      }
-
-      return $this->errorResponse($log);
+        return $this->errorResponse($log, 403);
     }
 
-    public function update(int $id) {
+    public function update(int $id)
+    {
+        $logModel = LogModel::findFirst($id);
 
-      $currentUser = $this->auth->data('user')->id;
+        if (!$logModel) {
+            return $this->errorResponse(new LogModel(), 404);
+        }
 
-      $logData = (array) $this->request->getJsonRawBody();
-      $logData['id'] = $id;
+        if ($logModel->user_id === $this->auth->data('user')->id || TeamModel::isTeamAdmin($logModel->project_id, $this->auth->data('user')->id)) {
+            $logData = (array) $this->request->getJsonRawBody();
+            $logData['id'] = $id;
 
-      $logModel = LogModel::findFirst($id);
+            if ($logModel->update($logData, ['start', 'end', 'description'])) {
+                return $this->successResponse($logModel, 200);
+            }
 
-      if (!$logModel) {
-        return $this->errorResponse(new LogModel(), 404);
-      }
+            return $this->errorResponse($logModel, 200);
+        }
 
-      if ($logModel->update($logData, ['start', 'end', 'description'])) {
-          return $this->successResponse($logModel, 200);
-      }
-      
-      return $this->errorResponse($logModel);
-
+        return $this->errorResponse(null, 403);
     }
 
     /**
      * Perform the deletion and return relevant response
      */
-    private function deleteLog(\Worklog\Models\Log $log) {
-      if ($log->delete()) {
-        return $this->successResponse();
-      }
+    private function deleteLog(\Worklog\Models\Log $log)
+    {
+        if ($log->delete()) {
+            return $this->successResponse();
+        }
 
-      return $this->errorResponse($log);
+        return $this->errorResponse($log);
     }
 }
